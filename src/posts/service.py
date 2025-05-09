3fe3ea5606014e0559bf4f2a0bb4467faf6330f9
nguyen minh dang <nguyenminhdangdev@gmail.com>
2025-05-09 11:30:44 +0700

    toiuu

--- src/posts/service.py
+++ src/posts/service.py
@@ -29,34 +29,7 @@ def du_lieu_ten(q: str) -> List[Nhan]:
     for row in rows:
         cols = row.select("td")
         if len(cols) >= 10:
-            try:
-                maunhan = cols[2].get_text(strip=True)
-                nhanhieu = cols[3].get_text(strip=True)
-                nhom = cols[4].get_text(strip=True)
-                status = cols[5].get_text(strip=True)
-                ngaynopdon_str = cols[6].get_text(strip=True)
-                ngaynopdon = datetime.strptime(ngaynopdon_str,
-                                               "%d/%m/%Y") if ngaynopdon_str and "/" in ngaynopdon_str else None
-                sodon = cols[7].get_text(strip=True)
-                chudon = cols[8].get_text(strip=True)
-                daidienshcn = cols[9].get_text(strip=True)
-
-                if nhanhieu:
-                    nhan_hieu.append(
-                        Nhan(
-                            maunhan=maunhan,
-                            nhanhieu=nhanhieu,
-                            nhom=nhom,
-                            status=status,
-                            ngaynopdon=ngaynopdon,
-                            sodon=sodon,
-                            chudon=chudon,
-                            daidienshcn=daidienshcn
-                        )
-                    )
-            except (ValueError, TypeError) as e:
-                logging.error(f"Lỗi khi xử lý dòng dữ liệu: {e}")
-                continue
+            nhan_hieu = luu_model(cols, nhan_hieu)
 
     return nhan_hieu
 
