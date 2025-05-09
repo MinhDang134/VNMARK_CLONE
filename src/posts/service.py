2c98652131741cf8ce9dd95b03c624d4cf3d0c8e
nguyen minh dang <nguyenminhdangdev@gmail.com>
2025-05-09 11:28:15 +0700

    toiuucode

--- src/posts/service.py
+++ src/posts/service.py
@@ -28,14 +28,15 @@ def du_lieu_ten(q: str) -> List[Nhan]:
     rows = soup.select("table tbody tr")
     for row in rows:
         cols = row.select("td")
-        if len(cols) >= 10 :
+        if len(cols) >= 10:
             try:
                 maunhan = cols[2].get_text(strip=True)
                 nhanhieu = cols[3].get_text(strip=True)
                 nhom = cols[4].get_text(strip=True)
                 status = cols[5].get_text(strip=True)
                 ngaynopdon_str = cols[6].get_text(strip=True)
-                ngaynopdon = datetime.strptime(ngaynopdon_str, "%d/%m/%Y") if ngaynopdon_str and "/" in ngaynopdon_str else None
+                ngaynopdon = datetime.strptime(ngaynopdon_str,
+                                               "%d/%m/%Y") if ngaynopdon_str and "/" in ngaynopdon_str else None
                 sodon = cols[7].get_text(strip=True)
                 chudon = cols[8].get_text(strip=True)
                 daidienshcn = cols[9].get_text(strip=True)
@@ -60,7 +61,7 @@ def du_lieu_ten(q: str) -> List[Nhan]:
     return nhan_hieu
 
 
-def du_lieu_theo_ngay(startday : str , endday : str):
+def du_lieu_theo_ngay(startday: str, endday: str):
     startday_fix = datetime.strptime(startday, "%Y-%m-%d")
     endday_fix = datetime.strptime(endday, "%Y-%m-%d")
     fd_param = quote_plus(f"{startday_fix.strftime('%d/%m/%Y')} - {endday_fix.strftime('%d/%m/%Y')}")
@@ -81,32 +82,37 @@ def du_lieu_theo_ngay(startday : str , endday : str):
     for row in rows:
         cols = row.select("td")
         if len(cols) >= 10:
-            try:
-                maunhan = cols[2].get_text(strip=True)
-                nhanhieu = cols[3].get_text(strip=True)
-                nhom = cols[4].get_text(strip=True)
-                status = cols[5].get_text(strip=True)
-                ngaynopdon_str = cols[6].get_text(strip=True)
-                ngaynopdon = datetime.strptime(ngaynopdon_str, "%d/%m/%Y") if ngaynopdon_str and "/" in ngaynopdon_str else None
-                sodon = cols[7].get_text(strip=True)
-                chudon = cols[8].get_text(strip=True)
-                daidienshcn = cols[9].get_text(strip=True)
-
-                if nhanhieu:
-                    nhan_hieu.append(
-                        Nhan(
-                            maunhan=maunhan,
-                            nhanhieu=nhanhieu,
-                            nhom=nhom,
-                            status=status,
-                            ngaynopdon=ngaynopdon,
-                            sodon=sodon,
-                            chudon=chudon,
-                            daidienshcn=daidienshcn
-                        )
-                    )
-            except (ValueError, TypeError) as e:
-                logging.error(f"Lỗi khi xử lý dòng dữ liệu: {e}")
-                continue
+            nhan_hieu = luu_model(cols,nhan_hieu)
 
     return nhan_hieu
+
+
+def luu_model(cols, dulieu: list):
+    try:
+        maunhan = cols[2].get_text(strip=True)
+        nhanhieu = cols[3].get_text(strip=True)
+        nhom = cols[4].get_text(strip=True)
+        status = cols[5].get_text(strip=True)
+        ngaynopdon_str = cols[6].get_text(strip=True)
+        ngaynopdon = datetime.strptime(ngaynopdon_str, "%d/%m/%Y") if ngaynopdon_str and "/" in ngaynopdon_str else None
+        sodon = cols[7].get_text(strip=True)
+        chudon = cols[8].get_text(strip=True)
+        daidienshcn = cols[9].get_text(strip=True)
+
+        if nhanhieu:
+            dulieu.append(
+                Nhan(
+                    maunhan=maunhan,
+                    nhanhieu=nhanhieu,
+                    nhom=nhom,
+                    status=status,
+                    ngaynopdon=ngaynopdon,
+                    sodon=sodon,
+                    chudon=chudon,
+                    daidienshcn=daidienshcn
+                )
+            )
+    except (ValueError, TypeError) as e:
+        logging.error(f"Lỗi khi xử lý dòng dữ liệu: {e}")
+
+    return dulieu
